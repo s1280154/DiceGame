0677ee2938ce170934e60269e0f6307ce270d688
s1280154 <s1280154@std4dc15>
2021-07-02 12:35:49 +0900

    Greetings added.

--- DiceGame.java
+++ DiceGame.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class DiceGame{
     public static void main(String[] args) throws Exception{
 	int a,b,sum;
 	a=(int)Math.floor(Math.random()*6+1);
 	b=(int)Math.floor(Math.random()*6+1);
 	sum=a+b;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("What is your name?");
+	String name=scanner.next();
+	System.out.println("Hello, "+name+"!");
 	System.out.println("Rolling the dice...");
 	System.out.println("Die1: "+a);
 	System.out.println("Die2: "+b);
